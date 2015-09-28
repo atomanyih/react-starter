c36d5a899240bc94cc22531bfc29db2aace19139
August Toman-Yih <atomanyih@pivotal.io>
2015-09-28 14:22:52 -0700

    use es6 plox

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-var React = require('react');
+const React = require('react');
 
-var App = React.createClass({
+const App = React.createClass({
   render() {
     return (
       <div className="app">
