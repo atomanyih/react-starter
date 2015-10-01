0e2fe30d2ceda701931250ec5473b738afdda0f6
August Toman-Yih <atomanyih@pivotal.io>
2015-10-01 10:17:18 -0700

    fix formatting

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,20 @@ var connect = require('gulp-connect');
 var plugins = require('gulp-load-plugins')();
 
 gulp.task('build-js', function() {
- return gulp.src('js/app.js')
-   .pipe(webpack({
-   module: {
-     loaders: [
-       {
-         test: /\.js$/,
-         exclude: /node_modules/,
-         loader: 'babel-loader?stage=0&optional[]=runtime&loose=true&nonStandard=true'
-       },
-     ],
-   },
- }))
- .pipe(rename('app.js'))
- .pipe(gulp.dest('dist/'));
+  return gulp.src('js/app.js')
+    .pipe(webpack({
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader?stage=0&optional[]=runtime&loose=true&nonStandard=true'
+          },
+        ],
+      },
+    }))
+    .pipe(rename('app.js'))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('setup-watchers', function(callback) {
@@ -27,7 +27,7 @@ gulp.task('setup-watchers', function(callback) {
   callback();
 });
 gulp.task('webserver', ['build-js'], function() {
- connect.server();
+  connect.server();
 });
 
 gulp.task('default', ['setup-watchers', 'webserver']);
@@ -47,7 +47,7 @@ gulp.task('jasmine', function() {
           }
         ]
       },
-      output: {filename: 'spec.js' },
+      output: {filename: 'spec.js'},
       plugins: [plugin]
     }))
     .pipe(plugins.jasmineBrowser.specRunner())
